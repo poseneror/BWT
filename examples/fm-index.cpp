96945c7f29d4d4d49640e0703d67bd6be9951c64
Or Posener <posener.or@gmail.com>
2019-04-18 19:56:08 +0300

    second

--- examples/fm-index.cpp
+++ examples/fm-index.cpp
@@ -1,55 +1,132 @@
+#include <iterator>
 #include <sdsl/suffix_arrays.hpp>
 #include <string>
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
-
 using namespace sdsl;
 using namespace std;
-
-int main(int argc, char** argv)
+// for (int i = 0; i < m; i++)
+// {
+//     for (int ci = 0; ci < sigma.length(); ci++)
+//     {
+//         char c = sigma.at(ci);
+//         if (c != query.at(i))
+//         {
+//             string modified = query.substr(0, i) + c + query.substr(i + 1);
+//             occs += count1(fm_index, modified.begin(), modified.end());
+//         }
+//     }
+// }
+template <class t_csa, class t_pat_iter>
+typename t_csa::size_type count1(
+    const t_csa &csa,
+    t_pat_iter begin,
+    t_pat_iter end,
+    csa_tag)
 {
-    if (argc <  2) {
+    if (end - begin > (typename std::iterator_traits<t_pat_iter>::difference_type)csa.size())
+        return 0;
+
+    // error is in first half:
+    typename t_csa::size_type l = 0;
+    typename t_csa::size_type r = csa.size() - 1;
+    string::iterator middle = begin;
+    advance(middle, ceil((end - begin) / 2));
+    //cout << "middle " << *begin << "begin " << begin << endl;
+    typename t_csa::size_type result = backward_search(csa, 0, csa.size() - 1, begin, end, l, r);
+    cout<<result<<endl;
+    typename t_csa::size_type total=0; 
+    if (!result) return 0;
+
+    for (string::iterator i = middle; i >= begin; i--)
+    {
+        result = backward_search(csa, l, r, i + 1, middle, l, r);
+        cout << result << endl;
+
+        if (!result)
+            return 0;
+            string sigma= "abcdefghijklmnopqrstuvwxyz";
+        
+        for (int ci = 0; ci < sigma.length(); ci++)
+        {
+            char c = sigma.at(ci);
+            if (c==*i) continue;
+            result = backward_search(csa, l, r, c, l, r);
+            cout << result << endl;
+
+            if (!result) return 0;
+            result = backward_search(csa, l, r, begin, i, l, r);
+            cout << result << endl;
+
+            total+=result;
+        
+        }
+    }
+
+    return total;
+}
+
+template <class t_csx, class t_pat_iter>
+typename t_csx::size_type count1(
+    const t_csx &csx,
+    t_pat_iter begin,
+    t_pat_iter end)
+{
+    typename t_csx::index_category tag;
+    return count1(csx, begin, end, tag);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
         cout << "Usage " << argv[0] << " text_file [max_locations] [post_context] [pre_context]" << endl;
         cout << "    This program constructs a very compact FM-index" << endl;
         cout << "    which supports count, locate, and extract queries." << endl;
         cout << "    text_file      Original text file." << endl;
-        cout << "    error_count    Exact number of errors in text." <<endl;
-        cout << "    max_locations  Maximal number of location to report." <<endl;
+        cout << "    error_count    Exact number of errors in text." << endl;
+        cout << "    max_locations  Maximal number of location to report." << endl;
         cout << "    post_context   Maximal length of the reported post-context." << endl;
         cout << "    pre_context    Maximal length of the pre-context." << endl;
         return 1;
     }
 
-    size_t error_count = 2;
+    size_t error_count = 0;
     size_t max_locations = 5;
     size_t post_context = 10;
     size_t pre_context = 10;
 
-    if (argc >= 3) {
+    if (argc >= 3)
+    {
         error_count = atoi(argv[2]);
     }
-    if (argc >= 4) {
+    if (argc >= 4)
+    {
         max_locations = atoi(argv[3]);
     }
-    if (argc >= 5) {
+    if (argc >= 5)
+    {
         post_context = atoi(argv[4]);
     }
-    if (argc >= 6) {
+    if (argc >= 6)
+    {
         pre_context = atoi(argv[5]);
     }
     string index_suffix = ".fm9";
-    string index_file   = string(argv[1])+index_suffix;
-    csa_wt<wt_huff<rrr_vector<127> >, 512, 1024> fm_index;
+    string index_file = string(argv[1]) + index_suffix;
+    csa_wt<wt_huff<rrr_vector<127>>, 512, 1024> fm_index;
 
-    if (!load_from_file(fm_index, index_file)) {
+    if (!load_from_file(fm_index, index_file))
+    {
         ifstream in(argv[1]);
-        if (!in) {
+        if (!in)
+        {
             cout << "ERROR: File " << argv[1] << " does not exist. Exit." << endl;
             return 1;
         }
-        cout << "No index "<<index_file<< " located. Building index now." << endl;
-        construct(fm_index, argv[1], 1); // generate index
+        cout << "No index " << index_file << " located. Building index now." << endl;
+        construct(fm_index, argv[1], 1);     // generate index
         store_to_file(fm_index, index_file); // save it
     }
     cout << "Index construction complete, index requires " << size_in_mega_bytes(fm_index) << " MiB." << endl;
@@ -57,40 +134,55 @@ int main(int argc, char** argv)
     string prompt = "\e[0;32m>\e[0m ";
     cout << prompt;
     string query;
-    while (getline(cin, query)) {
-        size_t m  = query.size();
+    while (getline(cin, query))
+    {
+        size_t m = query.size();
         // todo: here we count the occurrences
-        size_t occs = sdsl::count(fm_index, query.begin(), query.end());
-        cout << "# of occurrences: " << occs << endl;
-        if (occs > 0) {
-            cout << "Location and context of first occurrences: " << endl;
-            // todo: here we locate
-            auto locations = locate(fm_index, query.begin(), query.begin()+m);
-            sort(locations.begin(), locations.end());
-            for (size_t i = 0, pre_extract = pre_context, post_extract = post_context; i < min(occs, max_locations); ++i) {
-                cout << setw(8) << locations[i] << ": ";
-                if (pre_extract > locations[i]) {
-                    pre_extract = locations[i];
+        if (error_count == 0)
+        {
+            size_t occs = count(fm_index, query.begin(), query.end());
+            cout << "# of occurrences: " << occs << endl;
+            if (occs > 0)
+            {
+                cout << "Location and context of first occurrences: " << endl;
+                // todo: here we locate
+                auto locations = locate(fm_index, query.begin(), query.begin() + m);
+                sort(locations.begin(), locations.end());
+                for (size_t i = 0, pre_extract = pre_context, post_extract = post_context; i < min(occs, max_locations); ++i)
+                {
+                    cout << setw(8) << locations[i] << ": ";
+                    if (pre_extract > locations[i])
+                    {
+                        pre_extract = locations[i];
+                    }
+                    if (locations[i] + m + post_extract > fm_index.size())
+                    {
+                        post_extract = fm_index.size() - locations[i] - m;
+                    }
+                    auto s = extract(fm_index, locations[i] - pre_extract, locations[i] + m + post_extract - 1);
+                    string pre = s.substr(0, pre_extract);
+                    s = s.substr(pre_extract);
+                    if (pre.find_last_of('\n') != string::npos)
+                    {
+                        pre = pre.substr(pre.find_last_of('\n') + 1);
+                    }
+                    cout << pre;
+                    cout << "\e[1;31m";
+                    cout << s.substr(0, m);
+                    cout << "\e[0m";
+                    string context = s.substr(m);
+                    cout << context.substr(0, context.find_first_of('\n')) << endl;
                 }
-                if (locations[i]+m+ post_extract > fm_index.size()) {
-                    post_extract = fm_index.size()-locations[i]-m;
-                }
-                auto s   = extract(fm_index, locations[i]-pre_extract, locations[i]+m+ post_extract-1);
-                string pre = s.substr(0, pre_extract);
-                s = s.substr(pre_extract);
-                if (pre.find_last_of('\n') != string::npos) {
-                    pre = pre.substr(pre.find_last_of('\n')+1);
-                }
-                cout << pre;
-                cout << "\e[1;31m";
-                cout << s.substr(0, m);
-                cout << "\e[0m";
-                string context = s.substr(m);
-                cout << context.substr(0, context.find_first_of('\n')) << endl;
             }
+        }
+        else if (error_count == 1)
+        {
+            string sigma = "abcdefghijklmnopqrstvuwxyz"; //todo: find a way to achive # of abb
+            size_t occs = count1(fm_index, query.begin(), query.end());
+
+            cout << "# of occurrences: " << occs << endl;
         }
         cout << prompt;
     }
     cout << endl;
 }
-
